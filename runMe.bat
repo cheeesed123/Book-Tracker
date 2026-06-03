af1740c0d7396ce1e9850f9daffc0ddcf717a6d3
Chief Guy <olliessmith2910@gmail.com>
2026-06-03 03:03:24 -0400

    New update, new source code.

--- runMe.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-:: --- WINDOWS BATCH SECTION ---
-if "%1"=="1" ( 
-    echo Restart successful!
-    echo:
-)
-java -cp bin Runner
-::pause
--- runMe.sh
+++
@@ -1,6 +0,0 @@
-if [[ ${1:-0} -eq 1 ]]; then
-if [[ -d "bin" ]]; then
-	echo -e "Restart successful!\n"
-fi
-fi
-java -cp bin Runner
